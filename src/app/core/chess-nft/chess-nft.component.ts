c8a1088c68992d3662c4e9ed447c7dd9d5959692
Dalton Caron <dcaron@mtech.edu>
2022-04-21 00:56:25 -0700

    Working chess board render.

--- src/app/core/chess-nft/chess-nft.component.ts
+++ src/app/core/chess-nft/chess-nft.component.ts
@@ -1,19 +1,25 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { AfterViewInit, Component, Input, OnInit } from '@angular/core';
 
 const Chess = require('chess.js');
+const ChessBoard = require('chessboardjs/www/js/chessboard');
 
 @Component({
   selector: 'app-chess-nft',
   templateUrl: './chess-nft.component.html',
   styleUrls: ['./chess-nft.component.css']
 })
-export class ChessNFTComponent implements OnInit {
+export class ChessNFTComponent implements OnInit, AfterViewInit {
 
   @Input() public algebraicNotation: string = "";
   public chessLogic;
+  public chessDisplay: any;
 
   constructor() {
     this.chessLogic = Chess.Chess();
+  }
+
+  ngAfterViewInit(): void {
+    this.chessDisplay = ChessBoard('board1', 'start');
   }
 
   ngOnInit(): void {
